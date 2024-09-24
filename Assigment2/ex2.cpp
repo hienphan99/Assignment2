c523069276da255a965b23cad92c12e6f43b3466
HienPhan <80998096+hienphan99@users.noreply.github.com>
2024-09-24 16:05:42 +0700

    Update ex2.cpp

--- Assigment2/ex2.cpp
+++ Assigment2/ex2.cpp
@@ -12,15 +12,12 @@ void inputArrayExtend(int** p, int* n, int* maxSize) {
         if (input == '@') {
             break;
         }
-
         x = input - '0';
-
 
         if (x < 0 || x >= 10) {
             printf("So khong hop le, vui long nhap lai!\n");
             continue;
         }
-
 
         if (*n == *maxSize) {
             *maxSize *= 2;
@@ -30,46 +27,31 @@ void inputArrayExtend(int** p, int* n, int* maxSize) {
                 printf("Erro: Khong cap phat duoc bo nho!\n");
                 exit(1);
             }
-
-            // Sao chép d? li?u t? m?ng cu p sang m?ng m?i q
             for (int i = 0; i < *n; ++i) {
                 q[i] = (*p)[i];
             }
-            
-            // Gi?i phóng b? nh? m?ng cu
             free(*p);
-
-            // C?p nh?t con tr? p d? tr? t?i m?ng m?i q
             *p = q;
         }
-
-        // Thêm ph?n t? m?i vào m?ng
         (*p)[(*n)++] = x;
     }
 }
 
 int main() {
-    int maxSize = 2; // Kích thu?c ban d?u c?a m?ng
-    int n = 0;       // S? ph?n t? hi?n t?i trong m?ng
-    int* p = (int*)malloc(maxSize * sizeof(int)); // C?p phát d?ng m?ng p ban d?u
+    int maxSize = 2; 
+    int n = 0;      
+    int* p = (int*)malloc(maxSize * sizeof(int)); 
 
     if (p == NULL) {
         printf("\nError: Khong the cap phat bo nho!\n");
-        return 1; // Thoát n?u không th? c?p phát
+        return 1; 
     }
-
-    // G?i hàm d? nh?p d? li?u vào m?ng
     inputArrayExtend(&p, &n, &maxSize);
-
-    // In l?i các ph?n t? dã nh?p
     printf("Cac phan tu trong mang la: ");
     for (int i = 0; i < n; ++i) {
         printf("%d ", p[i]);
     }
     printf("\n");
-
-    // Gi?i phóng b? nh?
     free(p);
-
     return 0;
     }
